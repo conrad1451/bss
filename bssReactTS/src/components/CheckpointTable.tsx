fcbe00388f980ac2364bc1d602a1a19832f771aa
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-08-27 13:20:25 -0400

    fix: data type for mycheckpoint

--- bssReactTS/src/components/CheckpointTable.tsx
+++ bssReactTS/src/components/CheckpointTable.tsx
@@ -315,7 +315,7 @@ const CheckpointTable = (props: {
 
   // State variables for the "Add New Student" form
   const [myUsername, setMyUsername] = useState("");
-  const [myCheckpointData, setMyCheckpointData] = useState("");
+  const [myCheckpointData, setMyCheckpointData] = useState<string>("");
 
   // State for API call feedback
   const [loading, setLoading] = useState(false);
