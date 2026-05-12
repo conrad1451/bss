5bfb3d304723b8948002ae4db7a9099e989600a2
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-05-12 11:50:30 -0400

    feat: initialize mobData module for data-driven entity spawning
    Created data/mobData.js to house coordinate and attribute definitions for Mobs and Bosses. This supports the transition away from hardcoded entity instantiation in index.js.

---
+++ data/mobData.js
@@ -0,0 +1,121 @@
+export const mobDefinitions = [
+  // --- Regular Mobs ---
+  {
+    id: "lb_mushroom",
+    type: "ladybug",
+    field: "Mushroom",
+    pos: [10, 0, 15],
+    hp: 12,
+    lvl: 1,
+  },
+  {
+    id: "lb_clover",
+    type: "ladybug",
+    field: "Clover",
+    pos: [40, 0, -20],
+    hp: 20,
+    lvl: 2,
+  },
+  {
+    id: "lb_strawberry",
+    type: "ladybug",
+    field: "Strawberry",
+    pos: [-30, 0, 50],
+    hp: 28,
+    lvl: 3,
+  },
+
+  {
+    id: "rb_blueflower",
+    type: "rhino_beetle",
+    field: "Blue Flower",
+    pos: [-50, 0, -10],
+    hp: 10,
+    lvl: 1,
+  },
+  {
+    id: "rb_bamboo",
+    type: "rhino_beetle",
+    field: "Bamboo",
+    pos: [20, 0, 80],
+    hp: 25,
+    lvl: 2,
+  },
+  {
+    id: "rb_pineapple",
+    type: "rhino_beetle",
+    field: "Pineapple",
+    pos: [60, 0, 100],
+    hp: 34,
+    lvl: 5,
+  },
+
+  {
+    id: "spider_01",
+    type: "spider",
+    field: "Spider",
+    pos: [0, 0, 120],
+    hp: 100,
+    lvl: 4,
+  },
+  {
+    id: "mantis_01",
+    type: "mantis",
+    field: "Pineapple",
+    pos: [70, 0, 110],
+    hp: 115,
+    lvl: 4,
+  },
+  {
+    id: "werewolf_01",
+    type: "werewolf",
+    field: "Cactus",
+    pos: [-80, 0, 150],
+    hp: 250,
+    lvl: 7,
+  },
+
+  // --- Mini-Bosses ---
+  {
+    id: "vicious_01",
+    type: "vicious_bee",
+    field: "Spider",
+    pos: [5, 0, 125],
+    hp: 500,
+    lvl: 1,
+  }, // Level scales
+
+  // --- Bosses ---
+  {
+    id: "king_beetle",
+    type: "king_beetle",
+    field: "King Beetle Lair",
+    pos: [0, 0, 0],
+    hp: 2500,
+    lvl: 7,
+  },
+  {
+    id: "tunnel_bear",
+    type: "tunnel_bear",
+    field: "White Tunnel",
+    pos: [0, 0, 200],
+    hp: 10000,
+    lvl: 9,
+  },
+  {
+    id: "mondo_chick",
+    type: "mondo",
+    field: "Mountain Top",
+    pos: [0, 20, 300],
+    hp: 300000,
+    lvl: 8,
+  },
+  {
+    id: "coconut_crab",
+    type: "coconut_crab",
+    field: "Coconut",
+    pos: [100, 0, 400],
+    hp: 250000,
+    lvl: 12,
+  },
+];
