63a2f786d9753700c517874d93751a2b60fc5832
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-06-10 12:59:31 -0400

    chore: remove helper powershell scripts and tree printout

--- testscript1.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Path ".\theCleanedSVGs\*.txt" | ForEach-Object {
-    $target = ".\public\assets\$( (Get-Item $_.FullName).BaseName ).svg"
-    .\svgcleaningTXTtoSVG.ps1 -InputFile $_.FullName -OutputFile $target
-}
